c196341ef80b9344098fc27582d66e9220f9db9e
mroscardd <mroscardd@gmail.com>
2026-02-14 15:52:57 +0100

    emotion_analizer

---
+++ emotion_detection.py
@@ -0,0 +1,25 @@
+import requests 
+import json
+
+def emotion_detector(text_to_analyze):
+    """
+    Analiza las emociones presentes en un texto dado.
+
+    Recibe una cadena de caracteres y determina si el sentimiento predominante
+    es positivo, negativo o neutral
+
+    Args:
+        text_to_analyze (str): El texto que se someterá al análisis.
+
+    Returns:
+        json: Analisis de sentimientos
+    """
+    
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } } 
+    response = requests.post (url, json=input_json, headers=headers)
+
+    response_json = response.json()
+
+    return json.dumps(response_json, indent=4)
